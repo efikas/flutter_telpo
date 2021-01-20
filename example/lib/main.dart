6133e95babbed91ea13a7f7cf799fa382f7bc8bf
efikas01 <latyf01@gmail.com>
2021-01-20 15:10:36 +0100

    toast notifications added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,27 +19,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterTelpo.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -49,10 +28,65 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            Center(
+              child: Text('Running on: $_platformVersion\n'),
+            ),
+            ListTile(
+              leading: Icon(Icons.bluetooth_searching),
+              trailing:
+              IconButton(icon: Icon(Icons.print_rounded),),
+              title: Text("Default Printer"),
+              onTap: (){
+                TestPrintables().printThroughUsbPrinter();
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+class TestPrintables {
+  FlutterTelpo _printer = new FlutterTelpo();
+  bool _isConnected = false;
+
+
+  printThroughUsbPrinter() {
+    try {
+      _printer.connect();
+      _printer.isConnected().then((bool isConneted) {
+        if (isConneted) {
+          List<dynamic> _printables = [];
+
+          _printables.addAll([
+            PrintRow(
+              text: "BAUCHI STATE INTERNAL REVENUE SERVICE",
+              fontSize: 2,
+              position: 1,
+            ),
+            PrintRow(
+                text: "*****************************",
+                fontSize: 1,
+                position: 1),
+          ]);
+          _printables.addAll([
+            PrintRow(text: "TESTING", fontSize: 2, position: 1),
+            PrintRow(
+                text: "*****************************",
+                fontSize: 1,
+                position: 0),
+          ]);
+          _printables.add(WalkPaper(step: 10));
+          _printer.print(_printables.toList());
+        }
+      });
+    }
+    on PlatformException catch (e) {
+      print(e);
+    }
+  }
+}
+
